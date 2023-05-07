dc63a59f7755020aa54d12b72ebb5a3748a89138
zakymuhammad <arzaki666@gmail.com>
2023-05-07 18:30:53 +0700

    test1

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../index.css";
+
+const Login = () => {
+  return (
+    <div className="hero h-screen w-full flex justify-center items-center">
+      <div className="card-form w-[450px] h-80">
+        <form action=""></form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
