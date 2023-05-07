2097c6dc3f1265fcf9e986c2cefcd68f1f2e4bee
zakymuhammad <arzaki666@gmail.com>
2023-05-07 10:35:38 +0700

    test

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "../index.css";
+
+function Login() {
+  return (
+    <div className="hero h-screen w-full flex justify-center items-center">
+      <div className="card-form w-[450px] h-80">
+        <form action=""></form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
