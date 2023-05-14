6fa009472863777342a1c50490dcb5c38186faf8
zakymuhammad <arzaki666@gmail.com>
2023-05-14 10:32:34 +0700

    test-zky

--- frontend/src/components/historyAbsensi/Header.jsx
+++ frontend/src/components/historyAbsensi/Header.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque sequi
-        qui a cupiditate officia. Nisi itaque vero nihil quis debitis?
-      </p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Header;
