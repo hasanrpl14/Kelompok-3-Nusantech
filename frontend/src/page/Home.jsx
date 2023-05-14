d409a6a3d935c7bfd6013487a03fda4273f04ed4
zakymuhammad <arzaki666@gmail.com>
2023-05-14 10:05:13 +0700

    test-zky

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -3,29 +3,34 @@ import "../index.css";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex">
       {/* Sidebar */}
       <nav className="w-60 bg-gray-800 h-screen">
-      <div className="flex flex-col h-full w-1/6 bg-gray-800 text-white">
-  <div className="p-4">
-    <img src="D:/GITHUB/project nusntech/Kelompok-3-Nusantech/src/asset" alt="" className="h-12 w-12 mb-4" />
-  </div>
-  <ul className="flex flex-col">
-    <li className="p-4 rounded-xl hover:bg-gray-700 w-fit">
-      <a href="#">Dashboard</a>
-    </li>
-    <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
-      <a href="#">Data Siswa</a>
-    </li>
-    <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
-      <a href="#">Data Kelas</a>
-    </li>
-    <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
-      <a href="#">Data Absensi</a>
-    </li>
-  </ul>
-</div>
+        <div className="flex flex-col h-full w-1/6 bg-gray-800 text-white">
+          <div className="p-4">
+            <img
+              src="D:/GITHUB/project nusntech/Kelompok-3-Nusantech/src/asset"
+              alt=""
+              className="h-12 w-12 mb-4"
+            />
+          </div>
+          <ul className="flex flex-col">
+            <li className="p-4 rounded-xl hover:bg-gray-700 w-fit">
+              <a href="#">Dashboard</a>
+            </li>
+            <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
+              <a href="#">Data Siswa</a>
+            </li>
+            <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
+              <a href="#">Data Kelas</a>
+            </li>
+            <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
+              <a href="#">Data Absensi</a>
+            </li>
+          </ul>
+        </div>
       </nav>
 
       {/* Main Content */}
@@ -36,6 +41,12 @@ const Home = () => {
           onClick={() => navigate("/history")}
         >
           History
+        </button>
+        <button
+          className="p-10 ml-5 bg-sky-400 rounded-md"
+          onClick={() => navigate("/account")}
+        >
+          account
         </button>
       </div>
     </div>
