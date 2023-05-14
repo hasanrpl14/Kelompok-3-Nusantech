6a01df0067eab8bcefdcaccb27677f93b2078aa3
NiccKurangTidur <desheamaulana273@gmail.com>
2023-05-14 11:17:31 +0700

    benerin home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,8 @@ import Signin from "./components/login/Signin";
 import Signup from "./components/login/Signup";
 import Account from "./components/Halaman Account/Account";
 import Home from "./page/Home";
-<<<<<<< HEAD
 import History from "./page/History";
-=======
 import Tampilan from "./page/Tampilan";
->>>>>>> raffa
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -20,13 +17,10 @@ function App() {
             <Route path="/" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/home" element={<Home />} />
-<<<<<<< HEAD
             <Route path="/account" element={<Account/>} />
             <Route path="/history" element={<History />} />
-=======
             <Route path="/tampilan" element={<Tampilan />} />
 
->>>>>>> raffa
           </Routes>
         </Router>
       </header>
--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -6,32 +6,33 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <div className="flex">
-      {/* Sidebar */}
-      <nav className="w-60 bg-gray-800 h-screen">
-        <div className="flex flex-col h-full w-1/6 bg-gray-800 text-white">
-          <div className="p-4">
-            <img
-              src="D:/GITHUB/project nusntech/Kelompok-3-Nusantech/src/asset"
-              alt=""
-              className="h-12 w-12 mb-4"
-            />
-          </div>
-          <ul className="flex flex-col">
-            <li className="p-4 rounded-xl hover:bg-gray-700 w-fit">
-              <a href="#">Dashboard</a>
-            </li>
-            <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
-              <a href="#">Data Siswa</a>
-            </li>
-            <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
-              <a href="#">Data Kelas</a>
-            </li>
-            <li className="w-fit p-4 rounded-xl hover:bg-gray-700">
-              <a href="#">Data Absensi</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    {/* Sidebar */}
+    <nav className="w-60 bg-gray-800 h-screen">
+    <div className="flex flex-col h-full w-1/6 bg-gray-800 text-white">
+
+    <div className=" p-10 -mb-15">
+      <h1 className="text-3xl font-bold mb-4">Absensi</h1>
+     </div>
+
+<ul className="flex flex-col">
+  <li className="p-4 rounded-xl w-60 hover:bg-gray-700 ">
+    <a href="#">Dashboard</a>
+  </li>
+  <li className=" p-4 rounded-xl w-60 hover:bg-gray-700">
+    <a onClick={() => navigate('/account')}>Account</a>
+  </li>
+  <li className=" p-4 rounded-xl w-60 hover:bg-gray-700">
+    <a href="#">Data Kelas</a>
+  </li>
+  <li className="p-4 rounded-xl w-60 hover:bg-gray-700">
+    <a href="#">Data  </a>
+  </li>
+  <li className="p-4 rounded-xl w-60 hover:bg-gray-700">
+    <a >Data Absensi</a>
+  </li>
+</ul>
+</div>
+</nav>
       {/* Main Content */}
       <div className="flex-1 p-10">
         <button
