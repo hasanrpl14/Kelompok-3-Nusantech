d05dcc11f386207e7054a12172b118009096f827
Zackisaeful <zackisaefulbahri671@gmail.com>
2023-05-15 16:56:10 +0700

    t

---
+++ backend/middleware/AuthUser.js
@@ -0,0 +1,31 @@
+const User = require('../models/UserModel.js')
+
+// untuk mengecek apakah user itu sudah login atau belum
+exports.verifyUser = async (req, res, next) =>{
+    // ambil session jika tidak ada session maka munculkan pesan 
+    if(!req.session.userId){
+        return res.status(401).json({msg: "Mohon login ke akun Anda!"});
+    }
+    // ambil user berdasarkan uuid
+    const user = await User.findOne({
+        where: {
+            uuid: req.session.userId
+        }
+    });
+    // jika tidak ada munculkan pesan
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    req.userId = user.id;
+    req.role = user.role; 
+    next();
+}
+
+exports.adminOnly = async (req, res, next) =>{
+    const user = await User.findOne({
+        where: {
+            uuid: req.session.userId
+        }
+    });
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    if(user.role !== "admin") return res.status(403).json({msg: "Akses terlarang"});
+    next();
+}
